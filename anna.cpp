21697daf9eb6ccd49c6a47d9ca2341f13deeadca
mlacha <annahmlacha@gmail.com>
2025-03-18 11:37:15 +0300

    initialize variables

--- anna.cpp
+++ anna.cpp
@@ -1,3 +1,11 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class Loan {
+private:
+    string borrowerName;
+    double loanAmount;
+    double interestRate;
+    int loanTerm;  // Loan term in years
+    double totalAmountToRepay;
